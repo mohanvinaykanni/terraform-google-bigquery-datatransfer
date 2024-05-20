296d215b0e262f3c66618a833be5c9625dc62f5c
mohanvinaykanni <mohan.kanni@lloydsbanking.com>
2024-05-20 11:04:16 +0530

    Added Variable and tf vars

--- variable.tf
+++ variable.tf
@@ -1,13 +1,51 @@
 # Input variable definitions
-variable "destination_table_name_template" {
-  description = "Name of the s3 bucket. Must be unique."
+variable "destination_table_name" {
+  description = "Destination Table Name"
   type        = string
-  default = "terrafrom_gcstobq"
+  # default = "terrafrom_gcstobq"
 }
 
 variable "data_source_id" {
-  description = "Tags to set on the bucket."
+  description = "Contains Data Source Type , ex: GCS,Teradata etc"
   type        = string
   default     = "google_cloud_storage"
 }
 
+variable "data_destination_id" {
+  description = "Contains Bigquery Database Name"
+  type        = string
+  default     = "dev"
+}
+
+variable "transfer_job_name" {
+  description = "Name of the Job to Created with"
+  type = string
+}
+
+variable "encoding" {
+
+default = "UTF8"
+
+}
+
+variable "delimiter" {
+
+  default = ","
+  
+}
+
+variable "file_format" {
+  default = "CSV"
+}
+
+variable "service_account" {
+
+  default = "terraform@playpen-e3bd38.iam.gserviceaccount.com"
+  
+}
+
+variable "database_type" {
+
+  default = "Teradata"
+  
+}
